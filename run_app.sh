0a27b3f3ee92baae80466921bba46d2b2e1be669
Adrian <adriansandovalvargas@gmail.com>
2021-05-08 11:15:00 -0400

    lt link

--- run_app.sh
+++ run_app.sh
@@ -2,9 +2,7 @@ export FLASK_APP=Agriculta
 export FLASK_ENV=development
 export DEBUG=1
 export FLASK_RUN_PORT=5005
-# echo "localtunnel"
-# lt -h "https://testpi0101" --port 5005
-# echo "end local tunnel"
-# echo $link
+
+lxterminal --command="lt --port 5005 --subdomain agriculta"
 # flask run -h 192.168.1.155
 flask run

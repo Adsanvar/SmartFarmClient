c8aee9e2e9b56bb4b6415409c251853cca3e08a5
Adrian <adriansandovalvargas@gmail.com>
2021-05-08 11:19:51 -0400

    lt link

--- run_app.sh
+++ run_app.sh
@@ -3,6 +3,6 @@ export FLASK_ENV=development
 export DEBUG=1
 export FLASK_RUN_PORT=5005
 
-lxterminal --command="lt --port 5005 --subdomain agriculta"
+lxterminal -e 'bash "lt --port 5005 --subdomain agriculta"'
 # flask run -h 192.168.1.155
 flask run

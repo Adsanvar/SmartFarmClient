fe15732f3c7e5a3f98b849551952171e5a5dc43c
Adrian <adriansandovalvargas@gmail.com>
2021-05-11 15:39:49 -0400

    shell_scripts

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,11 @@
+
+val=$(tail -n 1 /etc/xdg/lxsession/LXDE-pi/autostart)
+if [ $val == "point-rpi" ]
+then
+    sudo sh -c "echo '@lxterminal -e ~/Documents/SmartFarmClient/run_app.sh' >> /etc/xdg/lxsession/LXDE-pi/autostart"
+    sudo sh -c "echo '@lxterminal -e ~/Documents/SmartFarmClient/localtunnel.sh' >> /etc/xdg/lxsession/LXDE-pi/autostart"
+fi
+
 export FLASK_APP=~/Documents/SmartFarmClient/Agriculta
 export FLASK_ENV=development
 export DEBUG=1

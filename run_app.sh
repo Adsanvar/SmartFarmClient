fa3d94cd0fa361b4ad957a50f5f9361bbf4401cc
Adrian <adriansandovalvargas@gmail.com>
2021-05-19 13:53:16 -0400

    camera test

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,14 @@
 
+timer=15
+
+echo "Application will start in: "
+while (($timer !=0))
+do
+    echo $timer
+    sleep 1s
+    timer=$((timer-1))
+done
+
 val=$(tail -n 1 /etc/xdg/lxsession/LXDE-pi/autostart)
 if [ $val == "point-rpi" ]
 then
@@ -10,7 +20,6 @@ export FLASK_APP=~/Documents/SmartFarmClient/Agriculta
 export FLASK_ENV=development
 export DEBUG=1
 export FLASK_RUN_PORT=5005
-
 # lxterminal -e bash --c "lt --port 5005 --subdomain agriculta"
-flask run
-# flask run --host 192.168.1.109
+# flask run
+flask run -h 192.168.1.109

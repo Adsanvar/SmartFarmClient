7335da621abdd0c298f722a2c979496f6d4d8c23
Adrian <adriansandovalvargas@gmail.com>
2021-05-19 12:56:58 -0400

    camera test

--- Agriculta/web_server.py
+++ Agriculta/web_server.py
@@ -6,13 +6,13 @@ import RPi.GPIO as GPIO
 from time import sleep
 from . import create_app
 import os
-import picamera
-import cv2
-import io
-import socket
+# import picamera
+# import cv2
+# import io
+# import socket
 
 home = Blueprint('home', __name__)
-vc = cv2.VideoCapture(0) 
+# vc = cv2.VideoCapture(0) 
 scheduler = APScheduler()
 scheduler.start()
 every=20
@@ -149,18 +149,18 @@ def activate_fan():
     print("Fan Finished")
     app.logger.info("FAN - ENDED: {}".format(now))
 
-def gen():
-    """Video streaming generator function."""
-    while True:
-        rval, frame = vc.read()
-        cv2.imwrite('t.jpg', frame)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+# def gen():
+#     """Video streaming generator function."""
+#     while True:
+#         rval, frame = vc.read()
+#         cv2.imwrite('t.jpg', frame)
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
 
 
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     """Video streaming route. Put this in the src attribute of an img tag."""
+#     return Response(gen(),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
 

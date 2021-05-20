d2be00301bfbf86d155804783544ece6f62d130b
Adrian <adriansandovalvargas@gmail.com>
2021-05-19 20:11:13 -0400

    beta test

--- Agriculta/web_server.py
+++ Agriculta/web_server.py
@@ -14,23 +14,20 @@ import os
 home = Blueprint('home', __name__)
 scheduler = APScheduler()
 scheduler.start()
-every=20
+every=15
 day = 24
-duration = 300
+duration = 180
 # light_duration = 64800 #ON
 light_duration_off = 14400 #OFF
 fan_duration_off = 21600 #OFF
 app = create_app()
 # vc = cv2.VideoCapture(0) 
 GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) # EXHAUST FAN
-# GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)  # LIGHTS
-# GPIO.setup(11, GPIO.OUT, initial=GPIO.HIGH) # FAN
-# GPIO.setup(13, GPIO.OUT, initial=GPIO.HIGH) # MISTER
-GPIO.setup(18, GPIO.OUT) # EXHAUST FAN
-GPIO.setup(16, GPIO.OUT)  # LIGHTS
-GPIO.setup(11, GPIO.OUT) # FAN
-GPIO.setup(13, GPIO.OUT) # MISTER
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW) # EXHAUST FAN
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)  # LIGHTS
+GPIO.setup(11, GPIO.OUT, initial=GPIO.HIGH) # FAN
+GPIO.setup(13, GPIO.OUT, initial=GPIO.HIGH) # MISTER
+
 #fan = LED(17, initial_value=True) #Set's it High (since our Relays are triggered on a low architecture)
 #mister = LED(27, initial_value=True)
 #This Route is the index page (landing page)

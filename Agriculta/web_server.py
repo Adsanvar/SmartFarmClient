09f2531abca1732cf41be9d8cc9ac921a054b3bf
Adrian <adriansandovalvargas@gmail.com>
2021-05-21 15:35:41 -0400

    new water cycle

--- Agriculta/web_server.py
+++ Agriculta/web_server.py
@@ -14,10 +14,10 @@ import os
 home = Blueprint('home', __name__)
 scheduler = APScheduler()
 scheduler.start()
-every=15
+every=3
 light_hours = 18
 every_fan_hours = 22
-duration = 300
+duration = 10
 # light_duration = 64800 #ON
 light_duration_off = 14400 #OFF
 fan_duration_on = 7200 #ON
@@ -164,12 +164,12 @@ def activate_mister():
 def activate_fan():
     now = datetime.datetime.now()
     # delta = now + datetime.timedelta(minutes = 1)
-    sleep(15)
+    # sleep(15)
     app.logger.info("FAN - START: {} ".format(now))
     print("{} - FAN Started".format(now))
     
     GPIO.output(11, GPIO.LOW)
-    sleep(duration+15)
+    sleep(duration+10)
     GPIO.output(11, GPIO.HIGH)
 
     print("Fan Finished")
